5327d6817b6cb29f8370770e87d16ac36cdcd0d7
Makesmart.net <github@makesmart.net>
2019-09-01 12:46:09 +0200

    Update client_2.ino

--- client_2.ino
+++ client_2.ino
@@ -1,16 +1,10 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
 #include <ESP8266HTTPClient.h>
 
-ESP8266WebServer server(80);
-HTTPClient http;
+HTTPClient sender;
 
 const char* ssid = "WLAN_SSID";
 const char* password = "WLAN_PASSWD";
-
-// following for the name of: http://name_esp.local/ 
-const char* dns_name = "sender";
 
 void setup()
 {
@@ -29,46 +23,28 @@ void setup()
 
   Serial.print("Verbunden! IP-Adresse: ");
   Serial.println(WiFi.localIP());
-
-  if (MDNS.begin(dns_name)) {
-    Serial.println("DNS gestartet, erreichbar unter: ");
-    Serial.println("http://" + String(dns_name) + ".local/");
-  }
-
-  server.onNotFound([](){
-    server.send(404, "text/plain", "Link wurde nicht gefunden!");  
-  });
-
-  server.on("/", []() {
-    server.send(200, "text/plain", "ESP-Startseite!");
-  });
-
-  server.begin();
-  Serial.println("Webserver gestartet.");
 }
 
 void loop() {
   
-  server.handleClient();
-  
+  //Wenn eine Verbindung mit dem oben definierten Netzwerk besteht
   if (WiFi.status() == WL_CONNECTED) {
  
     //Status des Relais vom Server abfragen
-    http.begin("http://192.168.178.67/get_data?wasserventil");
-    int http_code = http.GET();                                                                 
-    String returned_data = http.getString();
+    sender.begin("http://192.168.178.67/get_data?wasserventil");
+    sender.GET();                                                                 
+    String returned_data = sender.getString();
 
     Serial.println(returned_data);
-    Serial.println(http_code);
     
     if(returned_data == "1"){
-      http.begin("http://192.168.178.67/set_data?wasserventil=off"); 
-      int http_code = http.GET();   
+      sender.begin("http://192.168.178.67/set_data?wasserventil=off"); 
+      sender.GET();   
     }
 
     if(returned_data == "0"){
-      http.begin("http://192.168.178.67/set_data?wasserventil=on");
-      int http_code = http.GET(); 
+      sender.begin("http://192.168.178.67/set_data?wasserventil=on");
+      sender.GET(); 
     }
 
     //http.begin("http://192.168.178.67/set_data?wasserventil=on"); 
@@ -76,7 +52,7 @@ void loop() {
     //Der Anfrage 500ms Zeit geben, bevor die Verbindung zum Server wieder beendet wird
     delay(500);
      
-    http.end();
+    sender.end();
     
     
   }  
